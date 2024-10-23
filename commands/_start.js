4de57ce5cb1d971c33aacefe7eb4e7c1022fbec2
eubeljose <52637865+eubeljose@users.noreply.github.com>
2024-10-23 14:51:57 -0400

    Update _start.js

--- commands/_start.js
+++ commands/_start.js
@@ -15,5 +15,16 @@
  
 CMD*/
 
+const API_ID = '28494151'; // Reemplaza esto con tu API ID
+const API_HASH = 'ec36d21a3afd8a7e1a0223edf6d4555c'; // Reemplaza esto con tu API Hash
 
-bot.sendMessage(msg.chat.id, '¡Hola! Usa /buscar <nombre_parcial> para encontrar miembros.');
+const client = new TelegramClient(new StringSession(''), API_ID, API_HASH, { connection: 'tcp' });
+
+const GROUP_CHAT_ID = -1001192020135; // Reemplaza con el ID de tu grupo
+client.start();
+
+const TelegramBot = require('node-telegram-bot-api');
+const { TelegramClient } = require('telegram');
+const { StringSession } = require('telegram/sessions');
+
+bot.sendMessage('¡Hola! Usa /buscar <nombre_parcial> para encontrar miembros.');
