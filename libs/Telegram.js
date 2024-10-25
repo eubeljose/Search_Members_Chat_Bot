363bf229d40dfa698658917c568f8d0eafe9b333
eubeljose <52637865+eubeljose@users.noreply.github.com>
2024-10-24 20:06:43 -0500

    Update and rename telegram.js to Telegram.js

---
+++ libs/Telegram.js
@@ -0,0 +1,23 @@
+const { Api } = require('telegram');
+
+async function getParticipants(client, chatId) {
+  try {
+    const result = await client.invoke(
+      new Api.messages.GetParticipants({
+        channel: chatId,
+        filter: new Api.ChannelParticipantsRecent(),
+        offset: 0,
+        limit: 100,
+        hash: 0,
+      })
+    );
+
+    return result.participants;
+  } catch (error) {
+    console.log('Error fetching participants:', error);
+  }
+}
+
+module.exports = {
+  getParticipants
+};
